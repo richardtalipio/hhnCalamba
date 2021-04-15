495002be03f7ef61722dc03079d5c8d2111ee52e
richardtalipio <richardtalipio@gmail.com>
2021-04-15 14:26:23 +0800

    First Commit

---
+++ hhn-calamba-be/hhn/src/main/java/com/ally/hhn/HhnApplication.java
@@ -0,0 +1,13 @@
+package com.ally.hhn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhnApplication.class, args);
+	}
+
+}
---
+++ hhn-calamba-be/hhn/src/test/java/com/ally/hhn/HhnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ally.hhn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
